6ac79fa073ad18f4904eb8864645d86d0a9a53b2
Nicholas L Shrake <shraken@gmail.com>
2017-06-11 15:10:01 -0700

    make F380 default for gboto build, use IntelHex in python set_gboot_params script

--- gboot/scripts/set_gboot_params.py
+++ gboot/scripts/set_gboot_params.py
@@ -10,6 +10,7 @@ import argparse
 import sys
 import os
 import struct
+from intelhex import IntelHex
 
 GBOOT_CONFIG_OFFSET   = 0x2e    # (0x2e - 0x32) 4 byte
 GBOOT_EXT_INFO_OFFSET = 0x1e    # (0x1e - 0x22) 4 byte
@@ -18,6 +19,10 @@ GBOOT_PCB_REV_OFFSET  = 0x19    # (0x19 - 0x1a) 1 byte
 GBOOT_PID_OFFSET      = 0x16    # (0x16 - 0x18) 2 byte
 GBOOT_SERIAL_OFFSET   = 0x26    # (0x26 - 0x2a) 4 byte
 GBOOT_VER_OFFSET      = 0x18    # (0x18 - 0x19) 1 byte
+
+def write_hex_entry(ih, value_to_write, base_offset):
+    for count, entry in enumerate(value_to_write):
+            ih[base_offset + count] = ord(entry)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Set core parameters"
@@ -45,49 +50,43 @@ if __name__ == '__main__':
         print '==> error: gboot image could not be opened or does not exist.'
         sys.exit(1)
     else:
-        f = open(args.hex_file[0], 'r+b')
+        ih = IntelHex(args.hex_file[0])
 
+    # unimplemented in gflash (TODO: fix or remove)
     if args.config:
-        print '==> info: update config item at offset 0x%x' % (GBOOT_CONFIG_OFFSET)
-        config_value = struct.pack('<L', int(args.config[0]))
-        f.seek(GBOOT_CONFIG_OFFSET)
-        f.write(config_value)
+        print '==> info: update config item at offset 0x%04x' % (GBOOT_CONFIG_OFFSET)
+        config_value = struct.pack('<L', int(args.config[0], 16))
+        write_hex_entry(ih, config_value, GBOOT_CONFIG_OFFSET)
 
     if args.ext_info:
-        print '==> info: update ext info item at offset 0x%x' % (GBOOT_EXT_INFO_OFFSET)
-        ext_item_value = struct.pack('<L', int(args.ext_info[0]))
-        f.seek(GBOOT_EXT_INFO_OFFSET)
-        f.write(ext_item_value)
+        print '==> info: update ext info item at offset 0x%04x' % (GBOOT_EXT_INFO_OFFSET)
+        ext_item_value = struct.pack('<L', int(args.ext_info[0], 16))
+        write_hex_entry(ih, ext_item_value, GBOOT_EXT_INFO_OFFSET)
 
     if args.mcu:
-        print '==> info: update MCU item at offset 0x%x' % (GBOOT_MCU_OFFSET)
-        mcu_value = struct.pack('<H', int(args.mcu[0]))
-        f.seek(GBOOT_MCU_OFFSET)
-        f.write(mcu_value)
+        print '==> info: update MCU item at offset 0x%04x' % (GBOOT_MCU_OFFSET)
+        mcu_value = struct.pack('<H', int(args.mcu[0], 16))
+        write_hex_entry(ih, mcu_value, GBOOT_MCU_OFFSET)
 
     if args.pcb_rev:
-        print '==> info: update PCB revision item at offset 0x%x' % (GBOOT_PCB_REV_OFFSET)
-        pcb_rev_value = struct.pack('<B', int(args.pcb_rev[0]))
-        f.seek(GBOOT_PCB_REV_OFFSET)
-        f.write(pcb_rev_value)
+        print '==> info: update PCB revision item at offset 0x%04x' % (GBOOT_PCB_REV_OFFSET)
+        pcb_rev_value = struct.pack('<B', int(args.pcb_rev[0], 16))
+        write_hex_entry(ih, pcb_rev_value, GBOOT_PCB_REV_OFFSET)
 
     if args.pid:
-        print '==> info: update USB PID item at offset 0x%x' % (GBOOT_PID_OFFSET)
-        pcb_rev_value = struct.pack('<H', int(args.pid[0]))
-        f.seek(GBOOT_PID_OFFSET)
-        f.write(pcb_rev_value)
+        print '==> info: update USB PID item at offset 0x%04x' % (GBOOT_PID_OFFSET)
+        pid_value = struct.pack('<H', int(args.pid[0], 16))
+        write_hex_entry(ih, pid_value, GBOOT_PID_OFFSET)
 
     if args.serial:
-        print '==> info: update Serial ID item at offset 0x%x' % (GBOOT_SERIAL_OFFSET)
-        serial_value = struct.pack('<L', int(args.serial[0]))
-        f.seek(GBOOT_SERIAL_OFFSET)
-        f.write(serial_value)
+        print '==> info: update Serial ID item at offset 0x%04x' % (GBOOT_SERIAL_OFFSET)
+        serial_value = struct.pack('<L', int(args.serial[0], 16))
+        write_hex_entry(ih, serial_value, GBOOT_SERIAL_OFFSET)
 
     if args.version:
-        print '==> info: update Version item at offset 0x%x' % (GBOOT_VER_OFFSET)
-        version_value = struct.pack('<B', int(args.version[0]))
-        f.seek(GBOOT_VER_OFFSET)
-        f.write(version_value)
+        print '==> info: update Version item at offset 0x%04x' % (GBOOT_VER_OFFSET)
+        version_value = struct.pack('<B', int(args.version[0], 16))
+        write_hex_entry(ih, version_value, GBOOT_VER_OFFSET)
 
     print '==> info: finished updating gboot bootload image'
-    f.close()
+    ih.write_hex_file(args.hex_file[0])
